26c843393c615eb87d2053727e7297a26c3b3eab
Mohamed Elsayed <86891363+Mohamedzonkol@users.noreply.github.com>
2024-10-19 11:02:07 +0300

    Update providers.tf

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -1,24 +1,24 @@
 provider "azurerm" {
   alias           = "amr"
-  subscription_id = "8b9bfa48-1338-41a0-ba0b-1c84051e7e38"
-  client_id       = "4dafc491-7115-4c81-b3c5-2b58361ea397"
-  client_secret   = "YQC8Q~FwMbu3FIQyO_H5aj~xOsdirz2wI_uRob6f"
-  tenant_id       = "9db22b47-4c8f-4462-a115-9c07908b176c"
+  subscription_id = "8b9bfa48-1458-788a0-ba0b-1c84051e7e38"
+  client_id       = "4dafc491-7115-456a1-b3c5-2b58361ea397"
+  client_secret   = "YQC8Q~FwMbu3FI787_H5aj~xOsdirz2wI_uRob6f"
+  tenant_id       = "9db22b47-4c8f-458-a115-9c07908b176c"
 }
 
 provider "azurerm" {
   alias           = "zonkol"
-  subscription_id = "8712801a-11ce-432c-a9a6-f9a4bbbae20b"
-  client_id       = "632273ec-97c8-4edb-b174-7a86902cc960"
-  client_secret   = "Eyh8Q~JZ3xcaWtbird2TTEJLpfhBCJ6AhOsDQcbu"
-  tenant_id       = "9db22b47-4c8f-4462-a115-9c07908b176c"
+  subscription_id = "8712801a-11ce-4887-a9a6-f9a4bbbae20b"
+  client_id       = "632273ec-97c8-das7b-b174-7a86902cc960"
+  client_secret   = "Eyh8Q~JZ3xcaWtb1452TTEJLpfhBCJ6AhOsDQcbu"
+  tenant_id       = "9db22b47-4c8f-4897-a115-9c07908b176c"
 }
 
 
 provider "azurerm" {
   alias           = "tasneem"
-  subscription_id = "4aef1343-4d72-4946-ba9d-48548a0633bd"
-  client_id       = "a56f5ae6-172e-447f-be70-4f484add2e54"
-  client_secret   = "A8N8Q~BQLmelK2SLUh-XD_jH7U2YrS8cHpsGCbNY"
-  tenant_id       = "a09c2b8b-2c16-46e4-a53c-3f212d88fa53"
+  subscription_id = "4aef1343-4d72-478as-ba9d-48548a0633bd"
+  client_id       = "a56f5ae6-172e-4788-be70-4f484add2e54"
+  client_secret   = "A8N8Q~BQLmelK2SLUh-XD_asdU2YrS8cHpsGCbNY"
+  tenant_id       = "a09c2b8b-2c16-46e4-4573c-3f212d88fa53"
 }
